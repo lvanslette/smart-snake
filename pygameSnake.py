dd27ea358481e9e86eacb74241d8ba5f4dd69d1b
vansll <lvanslette>
2019-08-12 17:33:41 -0400

    created more functional snake game, still needs some work.

--- pygameSnake.py
+++ pygameSnake.py
@@ -1,37 +1,81 @@
 from pygame.locals import *
+from random import randint
 import pygame
-import pygamePlayer
+from pygamePlayer import Player
+import time
 
+class Apple:
+    x = 0
+    y = 0
+    step = 44
+
+    def __init__(self, x, y):
+        self.x = x * self.step
+        self.y = y * self.step
+
+    def draw(self, surface, image):
+        surface.blit(image, (self.x, self.y))
+
+class Game:
+    def isCollision(self, x1, y1, x2, y2, bsize):
+        if x1 >= x2 and x1 <= x2 + bsize:
+            if y1 >= y2 and y1 <= y2 + bsize:
+                return True
+        return False
 
 class App:
     windowWidth = 800
     windowHeight = 600
     player = 0
+    apple = 0
 
     def __init__(self):
         self._running = True
-        self._screen = None
-        self._image = None
-        self.player = pygamePlayer.Player()
+        self._display_surf = None
+        self._image_surf = None
+        self._apple_surf = None
+        self.game = Game()
+        self.player = Player(5)
+        self.apple = Apple(5, 5)
 
     def on_init(self):
         pygame.init()
-        self._screen = pygame.display.set_mode((self.windowWidth, self.windowHeight), pygame.HWSURFACE)
+        self._display_surf = pygame.display.set_mode((self.windowWidth, self.windowHeight), pygame.HWSURFACE)
 
         pygame.display.set_caption('Pygame pythonspot.com example')
         self._running = True
-        self._image = pygame.image.load("snakePiece.png").convert()
+        self._image_surf = pygame.image.load("snakePiece.png").convert()
+        self._apple_surf = pygame.image.load("apple.png").convert()
 
     def on_event(self, event):
         if event.type == QUIT:
             self._running = False
 
     def on_loop(self):
+        self.player.update()
+
+        # does snake eat apple?
+        # problem: snake eats at lower right hand side of apple, not apple
+        for i in range(0, self.player.length):
+            if self.game.isCollision(self.apple.x, self.apple.y, self.player.x[i], self.player.y[i], 44):
+                self.apple.x = randint(2, 9) * 44
+                self.apple.y = randint(2, 9) * 44
+                self.player.length = self.player.length + 1
+
+        # does snake collide with itself?
+        for i in range(2, self.player.length):
+            if self.game.isCollision(self.player.x[0], self.player.y[0], self.player.x[i], self.player.y[i], 40):
+                print("You lose! Collision: ")
+                print("x[0] (" + str(self.player.x[0]) + "," + str(self.player.y[0]) + ")")
+                print("x[" + str(i) + "] (" + str(self.player.x[i]) + "," + str(self.player.y[i]) + ")")
+                exit(0)
+
         pass
 
     def on_render(self):
-        self._screen.fill((0, 0, 0))
-        self._screen.blit(self._image, (self.player.x, self.player.y))
+        self._display_surf.fill((0, 0, 0))
+        self.player.draw(self._display_surf, self._image_surf)
+        self.apple.draw(self._display_surf, self._apple_surf)
         pygame.display.flip()
 
     def on_cleanup(self):
@@ -62,8 +106,9 @@ class App:
 
             self.on_loop()
             self.on_render()
-        self.on_cleanup()
 
+            time.sleep(50.0 / 1000.0)
+        self.on_cleanup()
 
 if __name__ == "__main__":
     theApp = App()
